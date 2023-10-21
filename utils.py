fdd0a124a05373f9403e8259ea9c0c649e30428d
Hengrui Bai <hengruibai@outlook.com>
2023-10-21 15:59:04 +1100

    ..

--- utils.py
+++ utils.py
@@ -33,6 +33,30 @@ def printMemory():
   print(f"total: {t}, reserved: {r}, free: {f}")
 
 
+def fdm_data(Smax, T, M, N, src, device):
+    data = pd.read_csv(src)
+    
+    ds = Smax/M
+    dt = T / N
+
+    input_data = []
+    output_data = []
+    for row_index, row in data.iterrows():
+        for col_index, value in enumerate(row):
+            input_data.append([(N-row_index)*dt, col_index*ds])  # Store row and column index as input
+            output_data.append([value])  # Store the corresponding value as output
+
+    X_train, X_test, y_train, y_test = train_test_split(input_data, output_data, test_size=0.2, random_state=42)
+
+    # Convert data to PyTorch tensors
+    X_train_tensor = torch.tensor(X_train, dtype=torch.float32).to(device)
+    y_train_tensor = torch.tensor(y_train, dtype=torch.float32).to(device)
+    X_test_tensor = torch.tensor(X_test, dtype=torch.float32).to(device)
+    y_test_tensor = torch.tensor(y_test, dtype=torch.float32).to(device)
+    
+    return X_train_tensor, y_train_tensor, X_test_tensor, y_test_tensor
+
+
 # This file generates training data
 def trainingData(K, r, sigma, T, Smax, S_range, t_range, gs, num_bc, num_fc, num_nc, RNG_key=None):
     '''
@@ -78,32 +102,6 @@ def trainingData(K, r, sigma, T, Smax, S_range, t_range, gs, num_bc, num_fc, num
     
     return bc_st_train, bc_v_train, n_st_train
 
-
-
-def fdm_data(Smax, T, M, N, src, device):
-    data = pd.read_csv(src)
-    
-    ds = Smax/M
-    dt = T / N
-
-    input_data = []
-    output_data = []
-    for row_index, row in data.iterrows():
-        for col_index, value in enumerate(row):
-            input_data.append([(N-row_index)*dt, col_index*ds])  # Store row and column index as input
-            output_data.append([value])  # Store the corresponding value as output
-
-    X_train, X_test, y_train, y_test = train_test_split(input_data, output_data, test_size=0.2, random_state=42)
-
-    # Convert data to PyTorch tensors
-    X_train_tensor = torch.tensor(X_train, dtype=torch.float32).to(device)
-    y_train_tensor = torch.tensor(y_train, dtype=torch.float32).to(device)
-    X_test_tensor = torch.tensor(X_test, dtype=torch.float32).to(device)
-    y_test_tensor = torch.tensor(y_test, dtype=torch.float32).to(device)
-    
-    return X_train_tensor, y_train_tensor, X_test_tensor, y_test_tensor
-
-
 def network_dispatcher(net, sizes, activation, dropout_rate, adaptive_rate, adaptive_rate_scaler):
     """
     Return network from `SUPPORTED_NETWORKS`.
@@ -139,8 +137,6 @@ def loss_dispatcher(pde_loss, bc_loss, data_loss, slope_recovery_term, loss_weig
     else:
         loss = loss_weights[0] * pde_loss + loss_weights[1] * bc_loss + loss_weights[2] * data_loss
     return loss
-
-
 
 def network_training(
         K, r, sigma, T, Smax, S_range, t_range, gs, num_bc, num_fc, num_nc, RNG_key,
@@ -284,164 +280,3 @@ def network_training(
     logging.info(f'Training finished. Elapsed time: {elapsed} s\n')
     
     return model, final_model, mse_loss_hist, pde_loss_hist, bc_loss_hist, data_loss_hist, loss_weights_hist
-
-
-
-# SUPPORTED_OPTIMIZERS = ['bfgs', 'sgd', 'adam']
-# def optimizer_dispatcher(optimizer, parameters, learning_rate):
-#     """Return optimization function from `SUPPORTED_OPTIMIZERS`.
-
-#     Parameters
-#     ----------
-#     optimizer : str
-#         Optimization function name
-#     parameters : callable
-#         Network parameters
-#     learning_rate : float
-#         Learning rate
-
-#     Returns
-#     -------
-#     callable
-#         Optimization function
-#     """
-#     assert isinstance(optimizer, (str, )), '`optimizer` type must be str.'
-#     optimizer = optimizer.lower()
-#     assert optimizer in SUPPORTED_OPTIMIZERS, 'Invalid optimizer. Falling to default.'
-#     if optimizer == 'bfgs':
-#         return torch.optim.LBFGS(parameters, line_search_fn="strong_wolfe")
-#     elif optimizer == 'sgd':
-#         return torch.optim.SGD(parameters, lr=learning_rate, momentum=0.9, nesterov=True, weight_decay=1e-2*learning_rate)
-#     else:
-#         return torch.optim.Adam(parameters, lr=learning_rate, betas=(0.9, 0.999), eps=1e-5)
-
-
-# def network_training(
-#         K, r, sigma, T, Smax, S_range, t_range, gs, num_bc, num_fc, num_nc, RNG_key,
-#         device, net, opt, sizes, activation, learning_rate, n_epochs, lossFunction,
-#         dropout_rate, adaptive_rate, adaptive_rate_scaler, loss_weights, adaptive_weight,
-#         X_train_tensor, y_train_tensor
-#         ):
-#     r"""Train PINN and return trained network alongside loss over time.
-
-#     Parameters
-#     ----------
-#     device : str
-#         Specifiy `cuda` if CUDA-enabled GPU is available, otherwise
-#         specify `cpu`
-#     domain : tuple or list
-#         Boundaries of the solution domain
-#     boundary_conditions : tuple or list
-#         Boundary conditions
-#     rhs : float
-#         Value of the scalar right hand side function
-#     sizes : list
-#         Each element represents the number of neuron per layer
-#     activation : callable 
-#         Activation function
-#     optimizer : callable
-#         Optimization procedure
-#     n_epochs : int
-#         The number of training epochs
-#     batch_size : int
-#         The number of data points for optimization per epoch
-#     linspace : bool
-#         Space the batch of data linearly, otherwise random
-#     learning_rate : float
-#         Learning rate
-#     dropout_rate : float, optional
-#         Dropout rate for regulrization during training process and
-#         uncertainty quantification by means of Monte Carlo dropout
-#         procedure while performing evaluation
-#     adaptive_rate : float, optional
-#         Scalable adaptive rate parameter for activation function that
-#         is added layer-wise for each neuron separately. It is treated
-#         as learnable parameter and will be optimized using a optimizer
-#         of choice
-#     adaptive_rate_scaler : float, optional
-#         Fixed, pre-defined, scaling factor for adaptive activation
-#         functions
-    
-#     Returns
-#     -------
-#     model : Net
-#         Trained function approximator
-#     loss_list : list
-#         Loss values during training process
-#     """
-    
-#     # initialize model and optimizer
-#     model = network_dispatcher(net, sizes, activation, dropout_rate, adaptive_rate, adaptive_rate_scaler).to(device=device)
-#     optimizer = optimizer_dispatcher(opt, model.parameters(), learning_rate)
-    
-#     # adaptive weight
-#     x_f_s = torch.tensor(0.).float().to(device).requires_grad_(True)
-#     x_label_s = torch.tensor(0.).float().to(device).requires_grad_(True)
-#     x_data_s = torch.tensor(0.).float().to(device).requires_grad_(True)
-#     optimizer_adam_weight = torch.optim.Adam([x_f_s] + [x_label_s] + [x_data_s], lr=learning_rate*10)
-    
-    
-#     # training
-#     loss_hist = []
-#     logging.info(f'{model}\n')
-#     logging.info(f'Training started at {datetime.datetime.now()}\n')
-#     min_train_loss = float("inf")  # Initialize with a large value
-#     final_model = None
-#     start_time = timer()
-    
-#     # training loop
-#     for _ in tqdm.tqdm(range(n_epochs), desc='[Training procedure]', ascii=True, total=n_epochs):
-
-#         # sampling
-#         bc_st_train, bc_v_train, n_st_train, n_v_train = \
-#             trainingData(K, r, sigma, T, Smax, S_range, t_range, gs, num_bc, num_fc, num_nc, RNG_key)
-#         # save training data points to tensor and send to device
-#         n_st_train = torch.from_numpy(n_st_train).float().requires_grad_().to(device)
-#         n_v_train = torch.from_numpy(n_v_train).float().to(device)
-                
-#         bc_st_train = torch.from_numpy(bc_st_train).float().to(device)
-#         bc_v_train = torch.from_numpy(bc_v_train).float().to(device)
-        
-#         # pde residual loss
-#         y1_hat = model(n_st_train)
-#         grads = tgrad.grad(y1_hat, n_st_train, grad_outputs=torch.ones(y1_hat.shape).cuda(), 
-#                     retain_graph=True, create_graph=True, only_inputs=True)[0]
-#         dVdt, dVdS = grads[:, 0].view(-1, 1), grads[:, 1].view(-1, 1)
-#         grads2nd = tgrad.grad(dVdS, n_st_train, grad_outputs=torch.ones(dVdS.shape).cuda(), 
-#                         create_graph=True, only_inputs=True, allow_unused=True)[0]
-#         S1 = n_st_train[:, 1].view(-1, 1)
-#         d2VdS2 = grads2nd[:, 1].view(-1, 1)
-#         pde_loss = lossFunction(-dVdt, 0.5*((sigma*S1)**2)*d2VdS2 + r*S1*dVdS - r*y1_hat)
-        
-#         # boudary condition loss
-#         y2_hat = model(bc_st_train)
-#         bc_loss = lossFunction(bc_v_train, y2_hat)
-        
-#         # data Round
-#         y3_hat = model(X_train_tensor)
-#         data_loss = lossFunction(y_train_tensor, y3_hat)
-        
-#         # calculate the total loss and update the model
-#         optimizer.zero_grad()
-#         loss = loss_dispatcher(pde_loss, bc_loss, data_loss, adaptive_rate, model, loss_weights, adaptive_weight, x_f_s, x_label_s, x_data_s)
-#         loss.backward()
-#         optimizer.step()
-        
-#         # update the weight if adaptive weight is used
-#         if adaptive_weight:
-#             optimizer_adam_weight.zero_grad()
-#             loss1 = torch.exp(-x_f_s) * pde_loss.detach() + x_f_s + torch.exp(-x_label_s) * bc_loss.detach() + x_label_s + torch.exp(-x_data_s) * data_loss.detach() + x_data_s
-#             loss1.backward()
-#             optimizer_adam_weight.step()
-#             pass
-        
-#         mse_loss = pde_loss + bc_loss + data_loss
-#         loss_hist.append(mse_loss.item())
-#         if mse_loss.item() < min_train_loss:
-#             min_train_loss = mse_loss.item()
-#             final_model = model.state_dict()
-#             pass
-#     elapsed = timer() - start_time
-#     logging.info(f'Training finished. Elapsed time: {elapsed} s\n')
-    
-#     return model, loss_hist, final_model
